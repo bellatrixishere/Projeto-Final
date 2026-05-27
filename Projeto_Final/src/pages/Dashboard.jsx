eb2da2243826b3e94c0c6ede6226b8b6fd893dd7
guirab734 <guiguicraft734@gmail.com>
2026-05-27 11:28:45 -0300

    feat: add pages

---
+++ Projeto_Final/src/pages/Dashboard.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Dashboard() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h2>Página de Dashboard (Painel do Chefe)</h2>
+      <p>teste</p>
+    </div>
+  );
+}
---
+++ Projeto_Final/src/pages/Gestao.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Gestao() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h2>Página de Gestão</h2>
+      <p>teste</p>
+    </div>
+  );
+}
